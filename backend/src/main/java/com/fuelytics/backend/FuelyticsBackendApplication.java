7315ae24ee52546ba8a5012ffbc93c2825458f41
David Cambra <cambrafontan.david@gmail.com>
2026-04-29 18:22:11 +0200

    feat: configurar backend Spring Boot y conexión inicial con PostgreSQL

---
+++ backend/src/main/java/com/fuelytics/backend/FuelyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fuelytics.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelyticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelyticsBackendApplication.class, args);
+	}
+
+}
